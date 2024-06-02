4308876cc4e6d89777f07cae3a21d8de42323792
VIPJoey <mmcorz@gmail.com>
2024-06-02 11:59:22 +0800

    2.0

---
+++ src/test/java/com/mmc/multi/kafka/starter/PbProcessor.java
@@ -0,0 +1,19 @@
+/*  
+ * Copyright (c) 2010-2030 Founder Ltd. All Rights Reserved.  
+ *  
+ * This software is the confidential and proprietary information of  
+ * Founder. You shall not disclose such Confidential Information  
+ * and shall use it only in accordance with the terms of the agreements  
+ * you entered into with Founder.  
+ *  
+ */ 
+ 
+package com.mmc.multi.kafka.starter;
+/**
+ * PbProcessor.
+ * @author tenkye 
+ * @date 2024/6/2 11:15
+ */
+public class PbProcessor {
+
+}
