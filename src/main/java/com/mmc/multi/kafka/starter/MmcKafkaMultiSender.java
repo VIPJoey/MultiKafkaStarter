e5a448eb9e96e538e11d79846829000d36e62e65
VIPJoey <mmcorz@gmail.com>
2024-06-23 16:27:43 +0800

    2.2

--- src/main/java/com/mmc/multi/kafka/starter/MmcKafkaMultiSender.java
+++ src/main/java/com/mmc/multi/kafka/starter/MmcKafkaMultiSender.java
@@ -18,7 +18,7 @@ import java.util.concurrent.atomic.AtomicLong;
 /**
  * MmcKafkaMultiSender.
  *
- * @author tenkye
+ * @author VIPJoey
  * @date 2024/6/23 16:18
  */
 public class MmcKafkaMultiSender implements MmcKafkaSender {
